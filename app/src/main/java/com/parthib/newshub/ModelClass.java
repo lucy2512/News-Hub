551c77a63c91caf77c9ed12d763819423ec36f75
lucy2512 <mail4parthib@gmail.com>
2022-07-13 21:12:25 +0530

    ModelClass Added

--- app/src/main/java/com/parthib/newshub/ModelClass.java
+++ app/src/main/java/com/parthib/newshub/ModelClass.java
@@ -1,18 +1,15 @@
 package com.parthib.newshub;
 
-public class ModelClass {
-
+public class ModelClass{
     private String author,title,description,url,urlToImage,publishedAt;
 
-    public ModelClass(String author,String title,String description,String url,String urlToImage,String publishedAt) {
-
+    public ModelClass(String author, String title, String description, String url, String urlToImage, String publishedAt) {
         this.author = author;
         this.title = title;
-        this.description=description;
-        this.url=url;
-        this.urlToImage=urlToImage;
-        this.publishedAt=publishedAt;
-
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
     }
 
     public String getAuthor() {
