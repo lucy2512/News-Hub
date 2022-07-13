c988bc0bbd0df230abe9c3672f2d26af46ba0900
lucy2512 <mail4parthib@gmail.com>
2022-07-13 21:10:25 +0530

    Sports Fragment Added

--- app/src/main/java/com/parthib/newshub/SportsFrag.java
+++ app/src/main/java/com/parthib/newshub/SportsFrag.java
@@ -8,13 +8,53 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SportsFrag extends Fragment {
+
+    String api="22d38fb887fd4b04886b0176ffe824f8";
+    ArrayList<ModelClass> modelClassArrayList;
+    private RecyclerView recyclerViewofsports;
+    Adapter adapter;
+    String country="in";
+    private String category="sports";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.sportsfrag,null);
+        recyclerViewofsports=v.findViewById(R.id.sportsf);
+        modelClassArrayList=new ArrayList<>();
+        recyclerViewofsports.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter=new Adapter(getContext(),modelClassArrayList);
+        recyclerViewofsports.setAdapter(adapter);
+
+        findNews();
+
         return v;
+    }
+
+    private void findNews() {
+        APIutils.getAPIinterface().getCategoryNews(country,category,100,api).enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if(response.isSuccessful()){
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
     }
 }
