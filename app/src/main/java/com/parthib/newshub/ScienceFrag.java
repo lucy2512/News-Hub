9a4b20d3a2c08e5f6e726a200a3cfac8d24a6203
lucy2512 <mail4parthib@gmail.com>
2022-07-13 21:09:26 +0530

    Science Frag Added

--- app/src/main/java/com/parthib/newshub/ScienceFrag.java
+++ app/src/main/java/com/parthib/newshub/ScienceFrag.java
@@ -8,13 +8,52 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ScienceFrag extends Fragment {
+    String api="22d38fb887fd4b04886b0176ffe824f8";
+    ArrayList<ModelClass> modelClassArrayList;
+    private RecyclerView recyclerViewofscience;
+    Adapter adapter;
+    String country="in";
+    private String category="science";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.sciencefrag,null);
+        recyclerViewofscience=v.findViewById(R.id.sciencef);
+        modelClassArrayList=new ArrayList<>();
+        recyclerViewofscience.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter=new Adapter(getContext(),modelClassArrayList);
+        recyclerViewofscience.setAdapter(adapter);
+
+        findNews();
+
         return v;
+    }
+
+    private void findNews() {
+        APIutils.getAPIinterface().getCategoryNews(country, category, 100, api).enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful()) {
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
     }
 }
