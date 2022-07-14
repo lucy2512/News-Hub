0686993138084692dfba638463f9a0fb12b0c67d
lucy2512 <mail4parthib@gmail.com>
2022-07-14 09:11:31 +0530

    Home Section Added

--- app/src/main/java/com/parthib/newshub/HomeFrag.java
+++ app/src/main/java/com/parthib/newshub/HomeFrag.java
@@ -8,13 +8,53 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFrag extends Fragment {
+
+    String api="22d38fb887fd4b04886b0176ffe824f8";
+    ArrayList<ModelClass> modelClassArrayList;
+    private RecyclerView recyclerViewofhome;
+    Adapter adapter;
+    String country="in";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.homefrag,null);
+
+        recyclerViewofhome=v.findViewById(R.id.homef);
+        modelClassArrayList=new ArrayList<>();
+        recyclerViewofhome.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter=new Adapter(getContext(),modelClassArrayList);
+        recyclerViewofhome.setAdapter(adapter);
+
+        findNews();
+
         return v;
+    }
+
+    private void findNews() {
+        APIutils.getAPIinterface().getNews(country,100,api).enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if(response.isSuccessful()){
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
     }
 }
