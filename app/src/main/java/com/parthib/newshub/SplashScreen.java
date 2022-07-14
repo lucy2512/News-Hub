f750c842a63d7cc3d931651c43c88ecc5145991f
lucy2512 <mail4parthib@gmail.com>
2022-07-14 09:11:09 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/parthib/newshub/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.parthib.newshub;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
