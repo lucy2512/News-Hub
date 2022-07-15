fe0b60f68f5165684467409fd2c8c71504e6914f
lucy2512 <mail4parthib@gmail.com>
2022-07-15 21:12:21 +0530

    Technology Section Added

--- app/src/main/java/com/parthib/newshub/TechFrag.java
+++ app/src/main/java/com/parthib/newshub/TechFrag.java
@@ -8,13 +8,53 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TechFrag extends Fragment {
+    String api="22d38fb887fd4b04886b0176ffe824f8";
+    ArrayList<ModelClass> modelClassArrayList;
+    private RecyclerView recyclerViewoftech;
+    Adapter adapter;
+    String country="in";
+    private String category="technology";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.techfrag,null);
+        recyclerViewoftech=v.findViewById(R.id.techf);
+        modelClassArrayList=new ArrayList<>();
+        recyclerViewoftech.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter=new Adapter(getContext(),modelClassArrayList);
+        recyclerViewoftech.setAdapter(adapter);
+
+        findNews();
+
         return v;
     }
+
+    private void findNews() {
+        APIutils.getAPIinterface().getCategoryNews(country,category,100,api).enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if(response.isSuccessful()){
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
+    }
+
 }
