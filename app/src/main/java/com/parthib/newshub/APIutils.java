82a5ad0517bc419bbe4ab9495bb154ccd3a0b015
lucy2512 <mail4parthib@gmail.com>
2022-07-15 21:11:56 +0530

    API Utils Added

--- app/src/main/java/com/parthib/newshub/APIutils.java
+++ app/src/main/java/com/parthib/newshub/APIutils.java
@@ -1,8 +1,19 @@
 package com.parthib.newshub;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIutils {
 
     private static Retrofit retrofit=null;
+
+    public static APIinterface getAPIinterface(){
+
+        if(retrofit==null){
+            retrofit=new Retrofit.Builder().baseUrl(APIinterface.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create()).build();
+
+        }
+        return retrofit.create(APIinterface.class);
+    }
 }
