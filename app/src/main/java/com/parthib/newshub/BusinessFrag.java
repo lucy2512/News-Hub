4884bf7a4f0af1bc7dcd3d700d222b84461dfb59
lucy2512 <mail4parthib@gmail.com>
2022-08-04 20:01:57 +0530

    Business Part Added

---
+++ app/src/main/java/com/parthib/newshub/BusinessFrag.java
@@ -0,0 +1,62 @@
+package com.parthib.newshub;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class BusinessFrag extends Fragment {
+
+    String api="22d38fb887fd4b04886b0176ffe824f8";
+    ArrayList<ModelClass> modelClassArrayList;
+    private RecyclerView recyclerViewofbusiness;
+    Adapter adapter;
+    String country="in";
+    private String category="business";
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.healthfrag,null);
+
+        recyclerViewofbusiness=v.findViewById(R.id.healthf);
+        modelClassArrayList=new ArrayList<>();
+        recyclerViewofbusiness.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter=new Adapter(getContext(),modelClassArrayList);
+        recyclerViewofbusiness.setAdapter(adapter);
+
+        findNews();
+
+        return v;
+    }
+
+    private void findNews() {
+        APIutils.getAPIinterface().getCategoryNews(country, category, 100, api).enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful()) {
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
+    }
+}
